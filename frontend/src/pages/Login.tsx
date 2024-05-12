ff5140c21d8f1b1673bdaca0e2c5c03078faf360
Harsika Kumari <kumariharsika8@gmail.com>
2024-05-12 23:41:50 +0530

    feat(toast): provide for sign IN

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,17 +1,26 @@
 import { IoIosLogIn } from "react-icons/io";
 import { Box, Button, Typography } from "@mui/material";
 import CustomizedInput from "../components/shared/CustomizedInput";
+import { useAuth } from "../context/AuthContext";
+import toast from "react-hot-toast";
 
 function Login() {
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const auth = useAuth();
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
-    const email = formData.get("email");
-    const password = formData.get("password");
-    console.log(email, password);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    try {
+      toast.loading("Signing In!", { id: "login" });
+      await auth?.login(email, password);
+      toast.success("Signed In Successfully!", { id: "login" });
+    } catch (error) {
+      toast.error("Signing In Failed", { id: "login" });
+      console.log(error);
+    }
   };
-  
+
   return (
     <Box width={"100%"} height={"100%"} display="flex" flex={1}>
       <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
@@ -26,7 +35,7 @@ function Login() {
         mt={16}
       >
         <form
-        onSubmit={handleSubmit}
+          onSubmit={handleSubmit}
           style={{
             margin: "auto",
             padding: "30px",
